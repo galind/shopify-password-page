d101659fc689ce5c14bbc06949b82b3650244d11
Guillem Galindo <64312665+galind@users.noreply.github.com>
2022-03-18 18:15:02 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+
+fetch('https://STORE_URL_HERE/password', {
+  method: 'POST',
+  body: JSON.stringify({
+      form_type: 'storefront_password',
+      utf8: '✓',
+      password: 'PASSWORD_HERE'}),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8'
+  }
+})
+.then(console.log)
